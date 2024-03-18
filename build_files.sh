e730dccf7e2ce73bd090c23f82148e96fdffe4c9
Arjun Acharya <acharayaarjun28@gmail.com>
2024-03-18 16:45:15 +0545

    Final Commit

--- build_files.sh
+++ build_files.sh
@@ -2,7 +2,7 @@
 
 echo "BUILD START"
 
-python3.10.5 -m pip install -r requirements.txt
-python3.10.5 manage.py collectstatic --noinput --clear
+python3.10 -m pip install -r requirements.txt
+python3.10 manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
